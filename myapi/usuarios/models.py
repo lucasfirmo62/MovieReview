9c39cee680f23f3c38bd9de16b5272bae14b3d48
marcos <marcosbezner@gmail.com>
2023-04-10 17:33:04 -0300

    feat: Adicionando as models do aplicativo

--- myapi/usuarios/models.py
+++ myapi/usuarios/models.py
@@ -10,3 +10,23 @@ class Usuario(models.Model):
     data_nascimento = models.DateField('Data de nascimento')
     email = models.EmailField(max_length=100)
     senha = models.CharField(max_length=100)
+    super_critico = models.BooleanField(default=False)
+
+AVALIACOES = [
+    (1,'1 - Horrível'),
+    (2,'2 - Ruim'),
+    (3,'3 - Mediano'),
+    (4,'4 - Bom'),
+    (5,'5 - Excelente')
+]
+
+
+class Publicacao(models.Model):
+
+    avaliacao = models.PositiveSmallIntegerField(choices=AVALIACOES)
+    text_pub = models.CharField(max_length=400)
+    usuario_cod_user = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    curtidas = models.PositiveIntegerField(default=0)
+    descurtidas = models.PositiveIntegerField(default=0)
+    comentario = models.CharField(max_length=200)
