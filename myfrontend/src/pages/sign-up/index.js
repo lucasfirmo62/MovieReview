5b5e3e46bccd825bd0bb6a4b86f0c3213f394b50
lucasfirmo62 <58527718+lucasfirmo62@users.noreply.github.com>
2023-04-12 15:32:53 -0300

    Conteúdo base atualizado

---
+++ myfrontend/src/pages/sign-up/index.js
@@ -0,0 +1,16 @@
+import './styles.css'
+import { Link } from 'react-router-dom';
+import api from '../../api';
+
+
+const SignUp = () => {
+
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default SignUp;
