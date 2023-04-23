1cebd11a8dca2126e381b6ee169004ade030a4df
lucasfirmo62 <lucasfirmo62@gmail.com>
2023-04-23 16:14:57 -0300

    feat: adicionando integracao do cadastro

--- myfrontend/src/pages/sign-up/index.js
+++ myfrontend/src/pages/sign-up/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import './styles.css'
 import { Link } from 'react-router-dom';
-import api from '../../api';
-
+import api from '../../api.js';
 
 const SignUp = () => {
 
@@ -41,6 +40,42 @@ const SignUp = () => {
         }
     }
 
+    async function testBorn(){
+        console.log(document.getElementById('date-born').value)
+    }
+
+    async function newUser() {
+        var full_name = document.getElementById('name').value
+        var nickname = document.getElementById('username').value
+        var bio_text = document.getElementById('user-bio').value
+        var birth_date = document.getElementById('date-born').value
+        var email = document.getElementById('email').value
+        var password = document.getElementById('confirmPass').value
+
+        const data = {
+            "full_name": full_name,
+            "nickname": nickname,
+            "bio_text": bio_text,
+            "birth_date": birth_date,
+            "email": email,
+            "super_reviewer": false,
+            "password": password
+          };
+          
+          api.post('/usuarios/', data, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(response => {
+            console.log('Usuário criado com sucesso!');
+          })
+          .catch(error => {
+            console.log('Erro ao criar usuário:', error);
+          });
+
+    }
+
 
     return (
         <>
@@ -50,19 +85,22 @@ const SignUp = () => {
                     <div className="content-box">
                         <p className="title-page">Cadastre-se</p>
                         <div className="space-input">
-                            <input className="content-input" placeholder="Nome completo" type="text" onChange={verifyName} />
+                            <input className="content-input" id="name" placeholder="Nome completo" type="text" onChange={verifyName} />
                             <p id="alert-name" className="alert-name">O Nome completo não mais de 60 caracteres</p>
                         </div>
                         <div className="space-input">
-                            <input className="content-input" placeholder="Username" type="text" />
+                            <input className="content-input" id="username" placeholder="Username" type="text" />
                             <p id="alert-username" className="alert-username">Esse Username já foi cadastrado</p>
                         </div>
                         <div className="space-input">
-                            <input className="content-input" placeholder="Email" type="text" />
+                            <input className="content-input" id="user-bio" placeholder="Sobre você" type="text" />
+                        </div>
+                        <div className="space-input">
+                            <input className="content-input" id="email" placeholder="Email" type="text" />
                             <p id="alert-email" className="alert-email">Esse Email já foi cadastrado</p>
                         </div>
                         <div className="space-input">
-                            <input className="content-input" type="date" />
+                            <input className="content-input" id="date-born" type="date" />
                             <p id="alert-date" className="alert-date">Este site apenas maiore de 13 anos</p>
                         </div>
                         <div className="input-several">
@@ -76,9 +114,9 @@ const SignUp = () => {
                         <p id="alert-password" className="alert-password">Senhas não coincidem</p>
                         <p id="alert-password-caracters" className="alert-password-caracters">A senha precisa ter mais de 7 caracteres</p>
                         <div className="space-input">
-                            <div id="button-simple" className="button-simple">
+                            <button id="button-simple" onClick={newUser} className="button-simple">
                                 <p>Criar Conta</p>
-                            </div>
+                            </button>
                         </div>
 
                         <div className="space-input">
