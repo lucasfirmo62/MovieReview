410d27bd6511c390304095d8bf1ed7667dad182b
lucasfirmo62 <lucasfirmo62@gmail.com>
2023-04-25 00:49:28 -0300

    feat: Tratando erros de validacao

--- myfrontend/src/pages/sign-up/index.js
+++ myfrontend/src/pages/sign-up/index.js
@@ -3,11 +3,14 @@ import './styles.css'
 import { Link } from 'react-router-dom';
 import api from '../../api.js';
 
-const SignUp = () => {
+import { useNavigate } from 'react-router-dom';
 
+const SignUp = () => {
+    const navigate = useNavigate();
 
     async function verifyName(character) {
-        if ((character.target.value.length > 60)) {
+        if ((character.target.value.length > 240)) {
+            document.getElementById("alert-name").innerHTML = "O campo precisa ter menos de 240 caracteres"
             document.getElementById("alert-name").style.display = "block"
             document.getElementById("button-simple").style.backgroundColor = "#ff51a8"
             document.getElementById("button-simple").style.cursor = "auto"
@@ -18,11 +21,48 @@ const SignUp = () => {
         }
     }
 
-    async function verifyPass(character) {
-        if (character.target.value != document.getElementById("password").value) {
-            document.getElementById("alert-password").style.display = "block"
+    async function verifyNickname(character) {
+        if ((character.target.value.length > 55)) {
+            document.getElementById("alert-username").innerHTML = "O campo precisa ter menos de 55 caracteres"
+            document.getElementById("alert-username").style.display = "block"
             document.getElementById("button-simple").style.backgroundColor = "#ff51a8"
             document.getElementById("button-simple").style.cursor = "auto"
+        } else {
+            document.getElementById("alert-username").style.display = "none"
+            document.getElementById("button-simple").style.backgroundColor = "#D30069"
+            document.getElementById("button-simple").style.cursor = "pointer"
+        }
+    }
+
+    async function verifyBio(character) {
+        if ((character.target.value.length > 220)) {
+            document.getElementById("alert-bio").innerHTML = "O campo precisa ter menos de 220 caracteres"
+            document.getElementById("alert-bio").style.display = "block"
+            document.getElementById("button-simple").style.backgroundColor = "#ff51a8"
+            document.getElementById("button-simple").style.cursor = "auto"
+        } else {
+            document.getElementById("alert-bio").style.display = "none"
+            document.getElementById("button-simple").style.backgroundColor = "#D30069"
+            document.getElementById("button-simple").style.cursor = "pointer"
+        }
+    }
+
+    async function verifyEmail(character) {
+        if ((character.target.value.length > 100)) {
+            document.getElementById("alert-email").innerHTML = "O campo precisa ter menos de 100 caracteres"
+            document.getElementById("alert-email").style.display = "block"
+            document.getElementById("button-simple").style.backgroundColor = "#ff51a8"
+            document.getElementById("button-simple").style.cursor = "auto"
+        } else {
+            document.getElementById("alert-email").style.display = "none"
+            document.getElementById("button-simple").style.backgroundColor = "#D30069"
+            document.getElementById("button-simple").style.cursor = "pointer"
+        }
+    }
+
+    async function verifyPass(character) {
+        if (character.target.value != document.getElementById("confirmPass").value) {
+            document.getElementById("alert-password").style.display = "block"
         } else {
             document.getElementById("alert-password").style.display = "none"
             document.getElementById("button-simple").style.backgroundColor = "#D30069"
@@ -31,8 +71,6 @@ const SignUp = () => {
 
         if (character.target.value.length < 7) {
             document.getElementById("alert-password-caracters").style.display = "block"
-            document.getElementById("button-simple").style.backgroundColor = "#ff51a8"
-            document.getElementById("button-simple").style.cursor = "auto"
         } else {
             document.getElementById("alert-password-caracters").style.display = "none"
             document.getElementById("button-simple").style.backgroundColor = "#D30069"
@@ -40,17 +78,79 @@ const SignUp = () => {
         }
     }
 
-    async function testBorn(){
-        console.log(document.getElementById('date-born').value)
-    }
+    // ele não pode enviar com os campos em branco 
+    // nenhum campo pode passar do limite de caracteres
+    // a verificação foi feita mas mesmo com erro ela passa e envia os dados e mesmo faltando campos.
+    // campos required: full_name, nickname, birth_date, email e password
+
+    const validateEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return(emailRegex.test(email));
+      };
 
     async function newUser() {
-        var full_name = document.getElementById('name').value
-        var nickname = document.getElementById('username').value
-        var bio_text = document.getElementById('user-bio').value
-        var birth_date = document.getElementById('date-born').value
-        var email = document.getElementById('email').value
-        var password = document.getElementById('confirmPass').value
+        const full_name = document.getElementById('name').value.trim();
+        const nickname = document.getElementById('username').value.trim();
+        const bio_text = document.getElementById('user-bio').value.trim();
+        const birth_date = document.getElementById('date-born').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const firstPassword = document.getElementById("firstPassword").value
+        const password = document.getElementById('confirmPass').value
+
+        document.getElementById("alert-name").style.display = "none"
+        document.getElementById("alert-username").style.display = "none"
+        document.getElementById("alert-date").style.display = "none"
+        document.getElementById("alert-password").style.display = "none"
+        document.getElementById("alert-email").style.display = "none"
+
+
+        if (full_name === "") {
+            alert("Por favor, preencha todos os campos obrigatórios.");
+            document.getElementById("alert-name").innerHTML = "Preencha este campo com seu nome completo"
+            document.getElementById("alert-name").style.display = "block"
+            return;
+        }
+        if (nickname === "") {
+            alert("Por favor, preencha todos os campos obrigatórios.");
+            document.getElementById("alert-username").innerHTML = "Preencha este campo com um nome de usuário"
+            document.getElementById("alert-username").style.display = "block"
+            return;
+        }
+        if (birth_date === "") {
+            alert("Por favor, preencha todos os campos obrigatórios.");
+            document.getElementById("alert-date").innerHTML = "Preencha com sua data de nascimento"
+            document.getElementById("alert-date").style.display = "block"
+            return;
+        }
+        if (password === "" || firstPassword === "") {
+            alert("Por favor, preencha todos os campos obrigatórios.");
+            document.getElementById("alert-password").innerHTML = "Senhas não coincidem"
+            document.getElementById("alert-password").style.display = "block"
+            return;
+        }
+        if (password != firstPassword) {
+            document.getElementById("alert-password").innerHTML = "Senhas não coincidem"
+            document.getElementById("alert-password").style.display = "block"
+            return;
+        }
+
+        if(validateEmail(email) === false){
+
+            document.getElementById("alert-email").innerHTML = "Digite um email válido"
+            document.getElementById("alert-email").style.display = "block"
+            return;
+        }
+
+
+        if (full_name.length > 60 || nickname.length > 60 || email.length > 60 || password.length > 60) {
+            alert("Alguns campos excederam o limite máximo de caracteres (60).");
+            return;
+        }
+
+        if (password.length < 7) {
+            alert("A senha deve ter no mínimo 7 caracteres.");
+            return;
+        }
 
         const data = {
             "full_name": full_name,
@@ -60,22 +160,27 @@ const SignUp = () => {
             "email": email,
             "super_reviewer": false,
             "password": password
-          };
-          
-          api.post('/usuarios/', data, {
-            headers: {
-              'Content-Type': 'application/json'
+        };
+
+        try {
+
+            await api.post('/usuarios/', data, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            navigate('/login');
+
+        } catch (error) {
+            if (error.response.data.email) {
+                document.getElementById("alert-email").style.display = "block"
             }
-          })
-          .then(response => {
-            console.log('Usuário criado com sucesso!');
-          })
-          .catch(error => {
-            console.log('Erro ao criar usuário:', error);
-          });
-
+            if (error.response.data.nickname) {
+                document.getElementById("alert-username").style.display = "block"
+            }
+        }
     }
-
 
     return (
         <>
@@ -84,35 +189,45 @@ const SignUp = () => {
                 <div className="container">
                     <div className="content-box">
                         <p className="title-page">Cadastre-se</p>
+
                         <div className="space-input">
                             <input className="content-input" id="name" placeholder="Nome completo" type="text" onChange={verifyName} />
-                            <p id="alert-name" className="alert-name">O Nome completo não mais de 60 caracteres</p>
+                            <p id="alert-name" className="alert-name">O Nome completo não pode ter mais de 60 caracteres</p>
                         </div>
+
                         <div className="space-input">
-                            <input className="content-input" id="username" placeholder="Username" type="text" />
-                            <p id="alert-username" className="alert-username">Esse Username já foi cadastrado</p>
+                            <input className="content-input" id="username" placeholder="Username" type="text" onChange={verifyNickname} />
+                            {/* <p id="alert-username" className="alert-username">Esse Username já foi cadastrado</p> */}
+                            <p id="alert-username" className="alert-username">O Username não pode ter mais de 60 caracteres</p>
                         </div>
+
                         <div className="space-input">
-                            <input className="content-input" id="user-bio" placeholder="Sobre você" type="text" />
+                            <input className="content-input" id="user-bio" placeholder="Sobre você" type="text" onChange={verifyBio}/>
+                            <p className="alert-bio" id="alert-bio"></p>
                         </div>
+
                         <div className="space-input">
-                            <input className="content-input" id="email" placeholder="Email" type="text" />
+                            <input className="content-input" id="email" placeholder="Email" type="text" onChange={verifyEmail}/>
                             <p id="alert-email" className="alert-email">Esse Email já foi cadastrado</p>
                         </div>
+
                         <div className="space-input">
                             <input className="content-input" id="date-born" type="date" />
-                            <p id="alert-date" className="alert-date">Este site apenas maiore de 13 anos</p>
+                            <p id="alert-date" className="alert-date"></p>
                         </div>
+
                         <div className="input-several">
                             <div className="password-side">
-                                <input id="password" className="content-input-half" placeholder="Senha" type="password" />
+                                <input id="firstPassword" className="content-input-half" placeholder="Senha" type="password" />
                             </div>
                             <div className="password-side">
                                 <input id="confirmPass" className="content-input-half" placeholder="Confirmar senha" type="password" onChange={verifyPass} />
                             </div>
                         </div>
+
                         <p id="alert-password" className="alert-password">Senhas não coincidem</p>
                         <p id="alert-password-caracters" className="alert-password-caracters">A senha precisa ter mais de 7 caracteres</p>
+
                         <div className="space-input">
                             <button id="button-simple" onClick={newUser} className="button-simple">
                                 <p>Criar Conta</p>
