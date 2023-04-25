b69ed3f09f0c63d2d0c0d8458c32f1a45e918009
lucasfirmo62 <lucasfirmo62@gmail.com>
2023-04-25 02:03:11 -0300

    feat: Adicionando integracao do login

--- myfrontend/src/pages/login/index.js
+++ myfrontend/src/pages/login/index.js
@@ -1,18 +1,63 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './styles.css'
-
+import api from '../../api'
+import { useNavigate } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
 
 const Login = () => {
 
+    const navigate = useNavigate();
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [loginOption, setLoginOption] = useState(1);
+
+    var loginItem = localStorage.getItem('tokenUser');
+
+    if (loginItem) {
+        return <Navigate to="/" />;
+    }
+
 
     async function handleSubmit(event) {
         event.preventDefault();
-        
+
+        document.getElementById("alert-login-email").style.display = "none"
+        document.getElementById("alert-login-pass").style.display = "none"
+
+
+        if((document.getElementById('email-login').value.trim()) === ""){
+            document.getElementById("alert-login-email").innerHTML = "Você não pode deixar o campo do email vazio"
+            document.getElementById("alert-login-email").style.display = "block"
+        }
+
+        if((document.getElementById('pass-login').value.trim()) === ""){
+            document.getElementById("alert-login-pass").innerHTML = "Você não pode deixar o campo da senha vazio"
+            document.getElementById("alert-login-pass").style.display = "block"
+        }
+
+        switch (loginOption) {
+            case '1':
+                const data = {
+                    email: email,
+                    password: password
+                }
+
+                try {
+                    const info_data = await api.post('/api/token/', data);
+
+                    localStorage.setItem('tokenUser', JSON.stringify(info_data.data.access));
+                    localStorage.setItem('idUser', JSON.stringify(info_data.data.id));
+                    navigate(`/`)
+
+                } catch (err) {
+                    alert('Falha no login, tente novamente.');
+                }
+
+        }
     }
-    
+
     return (
         <>
             <center>
@@ -28,8 +73,10 @@ const Login = () => {
                                     placeholder="Email"
                                     type="text"
                                     value={email}
+                                    id="email-login"
                                     onChange={e => setEmail(e.target.value)}
                                 />
+                                <p className="alert-login-email" id="alert-login-email"></p>
                             </div>
 
                             <div className="space-input">
@@ -38,18 +85,20 @@ const Login = () => {
                                     placeholder="Senha"
                                     type="password"
                                     value={password}
+                                    id="pass-login"
                                     onChange={e => setPassword(e.target.value)}
                                 />
+                                <p className="alert-login-pass" id="alert-login-pass"></p>
                             </div>
 
                             <div className="space-input">
-                                <button type="submit" className="button-simple">
+                                <button value={1} onClick={(e) => setLoginOption(e.currentTarget.value)} type="submit" className="button-simple">
                                     <p>Fazer Login</p>
                                 </button>
                             </div>
 
                             <div className="space-input">
-                                <p>Não tem uma conta? <Link to="/signup">Se cadastre aqui!</Link></p>
+                                <p>Não tem uma conta? <Link to="/sign-up">Se cadastre aqui!</Link></p>
                             </div>
 
                         </form>
