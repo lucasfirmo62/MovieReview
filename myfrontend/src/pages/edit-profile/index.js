2e7c83c6231a2c32c805bad7109b27e7820d9c81
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2023-04-26 10:08:25 -0300

    feat: adiciona verificacao da quantidade de caracteres e se o campo esta vazio antes de editar os dados do perfil

--- myfrontend/src/pages/edit-profile/index.js
+++ myfrontend/src/pages/edit-profile/index.js
@@ -51,6 +51,11 @@ const EditProfile = () => {
 
         const headers = { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' };
 
+        if (!user.full_name || !user.nickname || !user.bio_text) {
+            alert("Por favor, preencha todos os campos");
+            return;
+        }
+
         api.patch(`/usuarios/${id}/`, user, { headers })
             .then(response => {
                 console.log('Dados do usuário atualizados com sucesso:', response.data);
@@ -78,15 +83,15 @@ const EditProfile = () => {
                             <div className="conf-profile">
                                 <div className='row'>
                                     <p>Seu nome</p>
-                                    <input placeholder={user.full_name} id='name' className="content-input-edit" value={user.full_name} onChange={(event) => setUser({...user, full_name: event.target.value})} />
+                                    <input maxLength={240}  placeholder={user.full_name} id='name' className="content-input-edit" value={user.full_name} onChange={(event) => setUser({...user, full_name: event.target.value})} />
                                 </div>
                                 <div className='row'>
                                     <p>Seu username</p>
-                                    <input placeholder={user.nickname} id='username' className="content-input-edit" value={user.nickname} onChange={(event) => setUser({...user, nickname: event.target.value})} />
+                                    <input maxLength={55}  placeholder={user.nickname} id='username' className="content-input-edit" value={user.nickname} onChange={(event) => setUser({...user, nickname: event.target.value})} />
                                 </div>
                                 <div className='row'>
                                     <p>Sobre você</p>
-                                    <textarea placeholder={user.bio_text}  id="w3review" rows="4" cols="50" className="content-input-edit" value={user.bio_text} onChange={(event) => setUser({...user, bio_text: event.target.value})}> </textarea>
+                                    <textarea maxLength={220} placeholder={user.bio_text}  id="w3review" rows="4" cols="50" className="content-input-edit" value={user.bio_text} onChange={(event) => setUser({...user, bio_text: event.target.value})}> </textarea>
                                 </div>
                             </div>
                             <button className='button-simple' onClick={handleSaveChanges}><p>Concluir alterações</p></button>
