16a3e28f475106f4647dd5b2301504297499514a
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2023-05-08 10:58:20 -0300

    feat: adiciona funcionalidades de seguir e deixar de seguir

--- myapi/usuarios/views.py
+++ myapi/usuarios/views.py
@@ -1,11 +1,14 @@
 from rest_framework import status
+
+from rest_framework.decorators import action
+
 from rest_framework.response import Response
 from rest_framework import viewsets
 
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 
-from .models import User, Publication
+from .models import User, Publication, Connection
 from .serializers import UserSerializer, PublicationSerializer
 
 from .authentication import MyJWTAuthentication
@@ -16,9 +19,40 @@ class UserViewSet(viewsets.ModelViewSet):
     authentication_classes = [MyJWTAuthentication]
 
     def get_authenticators(self):
-        if self.request.method == 'POST':
+        if self.request.method == 'POST' and not self.request.path.endswith('/follow/') and not self.request.path.endswith('/unfollow/'):
             return []
         return super().get_authenticators()
+    
+    @action(detail=True, methods=['post'], url_path='follow', url_name='user-follow')
+    def follow(self, request, pk=None):
+        user_to_follow = self.get_object()
+        user = request.user
+
+        if user == user_to_follow:
+            return Response({'error': 'Você não pode seguir a si mesmo'}, status=status.HTTP_400_BAD_REQUEST)
+
+        connection_exists = Connection.objects.filter(usuario_alpha=user, usuario_beta=user_to_follow).exists()
+
+        if not connection_exists:
+            connection = Connection(usuario_alpha=user, usuario_beta=user_to_follow)
+            connection.save()
+        
+        return Response({'status': 'ok'})
+    
+    @action(detail=True, methods=['post'], url_path='unfollow', url_name='user-unfollow')
+    def unfollow(self, request, pk=None):
+        user_to_unfollow = self.get_object()
+        user = request.user
+                
+        if user == user_to_unfollow:
+            return Response({'error': 'Você não pode deixar de seguir a si mesmo'}, status=status.HTTP_400_BAD_REQUEST)
+
+        connection = Connection.objects.filter(usuario_alpha=user, usuario_beta=user_to_unfollow).first()
+        
+        if connection:
+            connection.delete()
+            
+        return Response({'status': 'ok'})
     
 class LogoutView(APIView):
     authentication_classes = [MyJWTAuthentication]
