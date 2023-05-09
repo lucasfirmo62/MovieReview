9253fac9f09d5006a11fdac12dd6c7109a07697a
lucasfirmo62 <lucasfirmo62@gmail.com>
2023-05-09 10:35:44 -0300

    feat: Adicionando pesquisa de usuario na header e na pagina de pesquisa

--- myfrontend/src/components/header/index.js
+++ myfrontend/src/components/header/index.js
@@ -1,19 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './styles.css';
 
 import { FaSearch } from 'react-icons/fa';
 
-import { Link } from 'react-router-dom';
-
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import logo from '../../assets/logotype.png'
 
 const Header = () => {
-
     const navigate = useNavigate();
-
     const [isSearchOpen, setIsSearchOpen] = useState(false);
+    const [searchType, setSearchType] = useState("movies");
 
     const handleSearchIconClick = () => {
         setIsSearchOpen(!isSearchOpen);
@@ -22,10 +19,50 @@ const Header = () => {
     function handleSubmit(event) {
         event.preventDefault();
 
-        const query = event.target.elements.search.value.replace(/\s+/g, '+'); 
+        const query = event.target.elements.search.value.replace(/\s+/g, '+');
 
-        navigate(`/search?query=${query}`);
+        if (searchType === "users") {
+            navigate(`/search?user=${query}`);
+        } else {
+            navigate(`/search?query=${query}`);
+        }
     }
+
+    useEffect(() => {
+        const addEventListeners = () => {
+            const moviesCheckbox = document.getElementById("movies");
+            const usersCheckbox = document.getElementById("users");
+
+            if (moviesCheckbox && usersCheckbox) {
+                moviesCheckbox.addEventListener("change", handleMoviesChange);
+                usersCheckbox.addEventListener("change", handleUsersChange);
+            }
+        };
+
+        const handleMoviesChange = () => {
+            const moviesCheckbox = document.getElementById("movies");
+            const moviesChecked = moviesCheckbox.checked;
+            setSearchType(moviesChecked ? "movies" : "users");
+        };
+
+        const handleUsersChange = () => {
+            const usersCheckbox = document.getElementById("users");
+            const usersChecked = usersCheckbox.checked;
+            setSearchType(usersChecked ? "users" : "movies");
+        };
+
+        addEventListeners();
+
+        return () => {
+            const moviesCheckbox = document.getElementById("movies");
+            const usersCheckbox = document.getElementById("users");
+
+            if (moviesCheckbox && usersCheckbox) {
+                moviesCheckbox.removeEventListener("change", handleMoviesChange);
+                usersCheckbox.removeEventListener("change", handleUsersChange);
+            }
+        };
+    }, []);
 
     return (
         <>
@@ -37,7 +74,7 @@ const Header = () => {
                             src={logo} alt="Logo do site"
                         />
                     </Link>
-                    <form 
+                    <form
                         onSubmit={handleSubmit}
                         className={isSearchOpen ? 'form-close' : 'form-open'}
                     >
@@ -47,12 +84,38 @@ const Header = () => {
                             onClick={handleSearchIconClick}
                         />
                         {isSearchOpen && (
-                            <input
-                                className={isSearchOpen ? 'input-open' : ''}
-                                type="text"
-                                placeholder="Pesquisar filmes"
-                                name="search"
-                            />
+                            <div className="contnet-config-search">
+                                <input
+                                    className={isSearchOpen ? 'input-open' : ''}
+                                    type="text"
+                                    placeholder="Pesquisar filmes"
+                                    name="search"
+                                />
+                                <div className="option-conf-search-back">
+                                    <label>
+                                        <input
+                                            type="radio"
+                                            name="searchType"
+                                            className="option-conf-search"
+                                            value="movies"
+                                            checked={searchType === "movies"}
+                                            onChange={() => setSearchType("movies")}
+                                        />
+                                        Filmes
+                                    </label>
+                                    <label>
+                                        <input
+                                            type="radio"
+                                            name="searchType"
+                                            className="option-conf-search"
+                                            value="users"
+                                            checked={searchType === "users"}
+                                            onChange={() => setSearchType("users")}
+                                        />
+                                        Usuários
+                                    </label>
+                                </div>
+                            </div>
                         )}
                     </form>
                 </div>
