ea20910a841d87ce8368eb6847afca9163750b60
morpheus6556 <ma.usv@hotmail.com>
2023-05-14 15:35:56 -0300

    Feat: listar favoritos
    ## O que foi feito
    
    Foi criado um botão no /profile chamado favoritos que leva a pagina de listagem dos filmes favoritos
    
    Foi criado também a pagina de listar os favoritos, com 4 exemplos de filmes que estão feitos de forma estática, o botão de desfavoritar tira o filme da lista, porém ao atualizar a pagina as modificações são ignoradas já que foram feitas de forma estática

---
+++ myfrontend/src/pages/favoritos/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Header from '../../components/header';
+import Menu from '../../components/menu';
+import styles from  './styles.css';
+
+const Favoritos = () => {
+  var [favoriteList,auxFavoriteList] = useState(['Filme 1','Filme 2','Filme 3','Filme 4']);
+
+  //função que desfavorita o filme e o faz sair da lista
+  const clickDesfavoritar = (index) => {
+    const updatedList = [...favoriteList];
+    updatedList.splice(index, 1);
+    auxFavoriteList(updatedList);
+  };
+
+
+  return (
+    <>
+      <Header />
+      <div className="content-page">
+        <div className="left-content">
+          <Menu/>
+        </div>
+        <div className="center-content">        
+            <div className = "title-content">
+                <big className = "title-component">Meus Filmes Favoritos</big>                
+            </div>
+            <div className = "favorite-content">
+              {favoriteList.map((movie, index) => 
+              (
+                  <div key={index} className="favoriteMovie-component">
+                      <div className="moviePicture">                      
+                        </div>
+                        <div className="movieInfo">
+                          <p className="movieTitle">{movie}</p>
+                        </div>
+                        <div className="button-content">
+                          <button className="favoriteButton" onClick={() => clickDesfavoritar(index)} >Desfavoritar</button>
+                      </div>
+                  </div>
+              ))}
+
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Favoritos;
