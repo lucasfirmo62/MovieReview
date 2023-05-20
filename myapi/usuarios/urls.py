a77d3e8293b2884943c8e3f2af991f496686d347
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2023-05-19 21:08:49 -0300

    feat: adiciona integracao da opcao de favoritar e desfavoritar um filme

--- myapi/usuarios/urls.py
+++ myapi/usuarios/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('favoritos/', FavoritesViewSet.as_view({'post': 'create', 'get': 'list'}), name='criar_favorito'),
     path('favoritos/<int:movie_id>/', FavoritesViewSet.as_view({'delete': 'destroy_by_movie_id'})),
+    path('favoritos/<int:movie_id>/is_movie_favorite/', FavoritesViewSet.as_view({'get': 'is_movie_favorite'})),
     path('usuarios/search/', UserViewSet.as_view({'get': 'search'}), name='user-search'),
 ]
