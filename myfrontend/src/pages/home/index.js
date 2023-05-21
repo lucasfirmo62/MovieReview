6abbe6cfe728e9e2905d708b5414a89ca4d97aa9
lucasfirmo62 <lucasfirmo62@gmail.com>
2023-05-21 12:24:14 -0300

    feat: adicionando listagem de publicacoes no feed

--- myfrontend/src/pages/home/index.js
+++ myfrontend/src/pages/home/index.js
@@ -6,8 +6,11 @@ import styles from './styles.css';
 import Publication from "../../components/Publication";
 import Header from "../../components/header";
 import Menu from "../../components/menu";
+import ViewPublication from "../../components/ViewPublication";
 
 const Home = () => {
+    const [publications, setPublications] = useState([]);
+
     const [windowSize, setWindowSize] = useState({
         width: window.innerWidth,
         height: window.innerHeight
@@ -28,11 +31,20 @@ const Home = () => {
         };
     }, [])
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get('https://api.npoint.io/f505aab832e19325c051');
+            setPublications(response.data.publications);
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <>
             <Header />
             <div className="content-home">
-                 {windowSize.width < 680
+                {windowSize.width < 680
                     ?
                     (
                         <Menu />
@@ -44,6 +56,18 @@ const Home = () => {
                 }
                 <div className="content-box-home">
                     <Publication />
+                    {publications.map((publication) => (
+                        <ViewPublication
+                            userName={publication.userName}
+                            userID={publication.userID}
+                            idPost={publication.idPost}
+                            idMovie={publication.idMovie}
+                            rating={publication.rating}
+                            critic={publication.critic}
+                            image={publication?.image}
+                            date={publication.date}
+                        />
+                    ))}
                 </div>
                 <div className="home-right-content">
 
