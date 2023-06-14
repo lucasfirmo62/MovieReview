62d6de340cc0299f864c5f558abff4fd163a80ea
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2023-06-14 18:57:42 -0300

    feat: adiciona integracao dos favoritos com paginacao

--- myfrontend/src/pages/favoritos/index.js
+++ myfrontend/src/pages/favoritos/index.js
@@ -109,7 +109,6 @@ const Favoritos = () => {
         </div>
         <div className="center-content">
           <div className="title-content">
-            <div className="title-content">
               <Link
                 className="back-btn"
                 to={'/profile/'}
@@ -118,7 +117,6 @@ const Favoritos = () => {
                 <MdArrowBack size={32} className="back-icon" />
               </Link>
               <h1 className="title-component">Filmes Favoritos</h1>
-            </div>
           </div>
           <div className="favorite-content">
             {favoriteList.map((movie, index) =>
