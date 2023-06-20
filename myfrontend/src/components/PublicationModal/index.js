39bd8181b6c0b3ce4a44ed36cbd8201ac0f5d0eb
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2023-06-20 09:56:03 -0300

    feat: adiciona integracao para a criacao de criticas no modal do filme

--- myfrontend/src/components/PublicationModal/index.js
+++ myfrontend/src/components/PublicationModal/index.js
@@ -42,7 +42,89 @@ const Publication = (props) => {
     }
 
     async function handleSubmit(event) {
-    
+        event.preventDefault();
+
+        let errorMsg = [];
+
+        if ((document.getElementById("review-text").value).length < 10) {
+            errorMsg.push("A crítica precisa ter mais de 10 caracteres")
+        }
+
+        if (selectedMovie === '') {
+            errorMsg.push("O filme precisa ser selecionado")
+        }
+
+        if (selectedReview === '') {
+            errorMsg.push("A nota precisa ser selecionada")
+        }
+
+        if (errorMsg.length > 0) {
+            const concatenatedMessage = errorMsg.join('\n');
+
+            toast.error(concatenatedMessage, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            return;
+        }
+
+        let id = localStorage.getItem("idUser");
+        id = id.substring(1, id.length - 1);
+        let token = localStorage.getItem("tokenUser");
+        token = token.substring(1, token.length - 1);
+
+        const currentDate = new Date();
+        const formattedDate = currentDate.toLocaleDateString();
+
+        const data = {
+            "review": selectedReview,
+            "pub_text": postText,
+            "user_id": parseInt(id),
+            "date": formattedDate,
+            "movie_id": selectedMovie.id,
+            "movie_title": selectedMovie.original_title,
+        };
+
+        const formData = new FormData();
+
+        setIsLoading(true);
+
+        formData.append('image', selectedFile);
+        formData.append('review', data.review);
+        formData.append('pub_text', data.pub_text);
+        formData.append('user_id', data.user_id);
+        formData.append('date', data.date);
+        formData.append('movie_id', data.movie_id);
+        formData.append('movie_title', data.movie_title);
+
+        const headers = { Authorization: `Bearer ${token}`, 'Content-Type': 'multipart/form-data' };
+
+        try {
+            const response = await api.post('/publicacoes/', formData, { headers });
+            console.log(response.data);
+
+            setMinLoadingTimePassed(false);
+
+            await delay(2000);
+
+            setMinLoadingTimePassed(true);
+
+            setIsLoading(false);
+            setShowConfirmation(true);
+
+            await delay(2000);
+
+            setShowConfirmation(false);
+        } catch (error) {
+            console.log(error);
+        }
+        window.location.reload();
     };
 
     return (
